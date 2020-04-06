8cc1b6e501cc8f7d493361d9779bfebff9e33c20
jack spicer <owlman1911@gmail.com>
2020-04-06 01:33:30 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgpackup',
+    version='0.1.0',
+    author='Owl Man',
+    author_email='owlman1911@gmail.com',
+    description='A utility for backing up PostgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/owlman1911/pgbackup',
+    packages=find_packages('src')
+)
